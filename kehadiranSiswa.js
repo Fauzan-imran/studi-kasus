7b2e6017ba363a8f837a9c9bd65366dd54c94a6e
MSI <fauzanimran314@gmail.com>
2026-07-20 15:30:38 +0800

    Kehadiran siswa

---
+++ kehadiranSiswa.js
@@ -0,0 +1,21 @@
+const kehadiran = [
+    true,
+    false,
+    true,
+    true,
+    false,
+    true
+];
+let jumlahHadir = 0;
+let jumlahTidakHadir = 0;
+for (let i = 0; i < kehadiran.length; i++) {
+    if (kehadiran[i] === false) {
+        jumlahTidakHadir++;
+        console.log("Siswa tidak hadir");
+    } else {
+        jumlahHadir++;
+        console.log("Siswa hadir");
+    }
+}
+console.log("Jumlah siswa hadir: " + jumlahHadir);
+console.log("Jumlah siswa tidak hadir: " + jumlahTidakHadir);
