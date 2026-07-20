310b057edbd3bd0be37c21c6f41160b7f119c918
MSI <fauzanimran314@gmail.com>
2026-07-20 15:39:26 +0800

    Daftar umur

---
+++ daftarUmur.js
@@ -0,0 +1,8 @@
+const umur = [8,13,17,20,25,11];
+for (let i = 0; i < umur.length; i++) {
+    if (umur[i] >= 17) {
+        console.log("boleh masuk");
+    }else {
+        console.log("tidak boleh masuk");
+    }
+}
