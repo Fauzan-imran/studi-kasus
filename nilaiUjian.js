8a3cef2d34a1fb6ec1b527533aa37ab46a1c151d
MSI <fauzanimran314@gmail.com>
2026-07-20 15:06:52 +0800

    nilai ujian

---
+++ nilaiUjian.js
@@ -0,0 +1,14 @@
+const nilai = [90,75,60,45,88];
+let jumlahLulus = 0;
+let jumlahTidakLulus = 0;
+for (let i = 0; i <nilai.length; i++) {
+    if (nilai[i] >= 75) {
+        console.log("Nilai ke-" + (i+1) + " : " + nilai[i] + " -> lulus");
+        jumlahLulus++;
+    } else {
+        console.log("Nilai ke-" + (i+1) + " : " + nilai[i] + " -> tidak lulus");
+        jumlahTidakLulus++;
+    }
+}
+console.log("Jumlah siswa lulus: " + jumlahLulus);
+console.log("Jumlah siswa tidak lulus: " + jumlahTidakLulus);
